514430b1cb114592a32ef663cc8ed0d5e7428736
jahidulalammishuk <jahiduk.alam13@gmail.com>
2020-10-27 19:17:05 +0600

    contact update

--- resources/views/frontend/pages/contact/index.blade.php
+++ resources/views/frontend/pages/contact/index.blade.php
@@ -32,70 +32,40 @@
 <div id="pl-37"  class="panel-layout" ><div id="pg-37-0"  class="panel-grid panel-has-style" ><div class="siteorigin-panels-stretch panel-row-style panel-row-style-for-37-0" style="padding: 10px 0; " data-stretch-type="full" data-overlay="true" data-overlay-color="#000000" ><div id="pgc-37-0-0"  class="panel-grid-cell" ><div id="panel-37-0-0-0" class="widget_text so-panel widget widget_custom_html panel-first-child panel-last-child" data-index="0" ><div style="text-align: center;" data-title-color="#443f3f" data-headings-color="#440000" class="widget_text panel-widget-style panel-widget-style-for-37-0-0-0" ><div class="textwidget custom-html-widget"><style>
 @import url('https://fonts.googleapis.com/css?family=Cabin+Sketch:700');
 </style> 
-{{-- <h1 style="font-family: 'Cabin Sketch', cursive;">
-Our Directors
-</h1></div></div></div></div></div></div>
 
-<div id="pg-37-1"  class="panel-grid panel-has-style" >
-  <div style="padding: 10px 0; " data-overlay="true" data-overlay-color="#000000" class="panel-row-style panel-row-style-for-37-1" >
-    <div id="pgc-37-1-0"  class="panel-grid-cell" ><div id="panel-37-1-0-0" class="so-panel widget widget_text panel-first-child" data-index="1" >
-      <div style="text-align: left;" data-title-color="#443f3f" data-headings-color="#443f3f" class="panel-widget-style panel-widget-style-for-37-1-0-0" >			
-        <div class="textwidget"><p><strong>Arif Hassan<br />
-            </strong>Managing Director @ Lira Group Ltd.<br />
-            Chairman @ Titas Knitwear Ltd.</p>
-            </div>
-          </div>
-        </div>
-        <div id="panel-37-1-0-1" class="so-panel widget widget_text panel-last-child" data-index="2" >
-          <div style="text-align: left;" data-title-color="#443f3f" data-headings-color="#443f3f" class="panel-widget-style panel-widget-style-for-37-1-0-1" >			
-            <div class="textwidget"><p><strong>MD Rakib Uddin Dhali<br />
-</strong>Chairman @ Lira Group Ltd.<br />
-Chairman @ Supti Sweater Ltd.</p>
-</div>
-</div></div></div>
-<div id="pgc-37-1-1"  class="panel-grid-cell" ><div id="panel-37-1-1-0" class="so-panel widget widget_text panel-first-child" data-index="3" >
-  <div style="text-align: left;" data-title-color="#443f3f" data-headings-color="#443f3f" class="panel-widget-style panel-widget-style-for-37-1-1-0" >			
-    <div class="textwidget"><p><strong>Chris Bortolotti<br />
-</strong>Director @ SINOEX LLC<br />
-Director @ Lira Group Ltd.</p>
-</div>
-</div></div>
-<div id="panel-37-1-1-1" class="so-panel widget widget_text" data-index="4" >
-  <div style="text-align: left;" data-title-color="#443f3f" data-headings-color="#443f3f" class="panel-widget-style panel-widget-style-for-37-1-1-1" >			
-    <div class="textwidget"><p><strong>Shihab Uddin Dhali<br />
-</strong>Director @ Lira Group Ltd.</p>
-</div>
-</div></div><div id="panel-37-1-1-2" class="so-panel widget widget_text panel-last-child" data-index="5" >
-  <div style="text-align: left;" data-title-color="#443f3f" data-headings-color="#443f3f" class="panel-widget-style panel-widget-style-for-37-1-1-2" >		
-    <div class="textwidget"><p><strong>Jamil Hassan<br />
-</strong>Director @ Lira Group Ltd.</p>
-</div>
-</div></div></div><div id="pgc-37-1-2"  class="panel-grid-cell" ><div id="panel-37-1-2-0" class="so-panel widget widget_text panel-first-child" data-index="6" >
-  <div style="text-align: left;" data-title-color="#443f3f" data-headings-color="#443f3f" class="panel-widget-style panel-widget-style-for-37-1-2-0" >			
-    <div class="textwidget"><p><strong>Mohammad Hanif<br />
-</strong>Vice Chairman @ Lira Group Ltd.<br />
-Vice Chairman @ Titas Knitwear Ltd.<br />
-Chairman @ LYNX Design Ltd.</p>
-</div>
-</div></div><div id="panel-37-1-2-1" class="so-panel widget widget_text panel-last-child" data-index="7" >
-  <div style="text-align: left;" data-title-color="#443f3f" data-headings-color="#443f3f" class="panel-widget-style panel-widget-style-for-37-1-2-1" >		
-    	<div class="textwidget"><p><strong>Mohammad Sumon<br />
-</strong>Director @ Lira Group Ltd.<br />
-Director @ LYNX Design Ltd.</p>
-</div>
-</div></div></div></div></div> --}}
 
 <div id="pg-37-2"  class="panel-grid panel-has-style" ><div class="siteorigin-panels-stretch panel-row-style panel-row-style-for-37-2" 
   style="background-image: url(/frontend/wp-content/uploads/Office.jpg);padding: 100px 0; " 
   data-stretch-type="full" data-hasbg="hasbg" data-overlay="true" data-overlay-color="#202020" >
   <div id="pgc-37-2-0"  class="panel-grid-cell" >
+
     <div id="panel-37-2-0-0" class="so-panel widget widget_sydney_contact_info sydney_contact_info_widget panel-first-child" 
     data-index="8" ><div style="text-align: center;" id="c1" data-title-color="#ffffff" data-headings-color="#ffffff" class="panel-widget-style panel-widget-style-for-37-2-0-0" >
-      <h3 class="widget-title">Bangladesh Office</h3><div class="contact-address"><span><i class="sydney-svg-icon">
+      <h3 class="widget-title"> Office</h3>
+      <div class="contact-address">
+        <span><i class="sydney-svg-icon">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-          <path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z" />
-        </svg></i></span>House#172, Eastern Road, Baridhara DOHS, Dhaka 1206, Bangladesh.</div><div class="contact-phone"><span><i class="sydney-svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z" /></svg></i></span>Tel # 0088-02-8411163-4, Fax # 0088-02-8410587</div><div class="contact-email"><span><i class="sydney-svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z" /></svg></i></span><a href="mailto:&#100;&#104;&#97;&#108;&#105;&#64;&#106;&#100;k&#102;as&#104;i&#111;n.co&#109;">&#100;&#104;&#97;&#108;&#105;&#64;&#106;&#100;k&#102;as&#104;i&#111;n.co&#109;</a></div></div></div><div id="panel-37-2-0-1" class="so-panel widget widget_sydney_contact_info sydney_contact_info_widget" data-index="9" ><div style="text-align: center;" id="c2" data-title-color="#ffffff" data-headings-color="#ffffff" class="panel-widget-style panel-widget-style-for-37-2-0-1" ><h3 class="widget-title">USA Office</h3><div class="contact-address"><span><i class="sydney-svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z" /></svg></i></span>86-75 Midland Parkway, Apt # 6B, Jamaica, N.Y. 11432, USA.</div><div class="contact-phone"><span><i class="sydney-svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z" /></svg></i></span>Tel# 001-718-5262033, Cell# 001-917-6980953, Fax# 001-718-5262033</div><div class="contact-email"><span><i class="sydney-svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z" /></svg></i></span><a href="mailto:kamal&#64;&#106;&#100;&#107;&#102;&#97;sh&#105;&#111;&#110;&#46;com">kamal&#64;&#106;&#100;&#107;&#102;&#97;sh&#105;&#111;&#110;&#46;com</a></div></div></div><div id="panel-37-2-0-2" class="so-panel widget widget_sydney_contact_info sydney_contact_info_widget panel-last-child" data-index="10" ><div style="text-align: center;" id="c3" data-title-color="#ffffff" data-headings-color="#ffffff" class="panel-widget-style panel-widget-style-for-37-2-0-2" ><h3 class="widget-title">Spain Office</h3><div class="contact-address"><span><i class="sydney-svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z" /></svg></i></span>Calle Albacete 42, Valencia 46007</div><div class="contact-phone"><span><i class="sydney-svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z" /></svg></i></span>0034 622533554</div><div class="contact-email"><span><i class="sydney-svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z" /></svg></i></span><a href="mailto:&#104;anif&#64;j&#100;kf&#97;&#115;&#104;&#105;&#111;n.&#99;&#111;&#109;">&#104;anif&#64;j&#100;kf&#97;&#115;&#104;&#105;&#111;n.&#99;&#111;&#109;</a></div></div></div></div><div id="pgc-37-2-1"  class="panel-grid-cell" ><div id="panel-37-2-1-0" class="so-panel widget widget_text panel-first-child panel-last-child" data-index="11" ><div style="text-align: center;" data-title-color="#ffffff" data-headings-color="#ffffff" class="panel-widget-style panel-widget-style-for-37-2-1-0" ><h3 class="widget-title">Contact Us</h3>			<div class="textwidget"><div role="form" class="wpcf7" id="wpcf7-f394-p37-o1" lang="en-US" dir="ltr">
-<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
+        </svg></i></span>House#40, Flat-B2, Road#18, Sector #11, Uttara, Dhaka-1230,Bangladesh.</div>
+        <div class="contact-phone"><span>
+          <i class="sydney-svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+         </svg></i></span>Tel # +8801677577142, Fax # 0088-02-8410587</div><div class="contact-email">
+           <span><i class="sydney-svg-icon">
+             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            </svg>
+          </i></span><a href="">
+            </a></div></div></div>
+
+            
+
+                                        </div>
+                                          <div id="pgc-37-2-1"  
+                                            class="panel-grid-cell" >
+                                            <div id="panel-37-2-1-0" class="so-panel widget widget_text panel-first-child panel-last-child" data-index="11" >
+
+                                              <div style="text-align: center;" data-title-color="#ffffff" data-headings-color="#ffffff" class="panel-widget-style panel-widget-style-for-37-2-1-0" >
+                                                <h3 class="widget-title">Contact Us</h3>			<div class="textwidget"><div role="form" class="wpcf7" id="wpcf7-f394-p37-o1" lang="en-US" dir="ltr">
+<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul>
+</div>
 <form action="" method="post" class="wpcf7-form init cf7-style transparent-two-columns" novalidate="novalidate" data-status="init">
 <div style="display: none;">
 <input type="hidden" name="_wpcf7" value="394" />
